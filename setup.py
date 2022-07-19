dedc6850120537b8145a75b324f5633af5f27f2f
Sebastián Díaz <kziete@gmail.com>
2022-07-19 15:56:38 -0400

    trash

--- setup.py
+++ setup.py
@@ -1,26 +1,22 @@
 from setuptools import setup, find_packages
 
-VERSION = '0.0.1' 
-DESCRIPTION = 'Queues client'
-LONG_DESCRIPTION = 'Queues client'
+VERSION = "0.0.1"
+DESCRIPTION = "Queues client"
+LONG_DESCRIPTION = "Queues client"
 
 # Setting up
 setup(
-       # the name must match the folder name 'verysimplemodule'
-        name="queues_client", 
-        version=VERSION,
-        author="Sebastían Díaz",
-        author_email="<kziete@gmail.com>",
-        description=DESCRIPTION,
-        long_description=LONG_DESCRIPTION,
-        packages=find_packages(),
-    install_requires=[
-        "boto3"
-    ], # add any additional packages that 
-        # needs to be installed along with your package. Eg: 'caer'
-        
-        keywords=['python', 'queue', 'sqs'],
-        classifiers= [
-            "Development Status :: 3 - Alpha",
-        ]
+    # the name must match the folder name 'verysimplemodule'
+    name="queues_client",
+    version=VERSION,
+    author="Sebastían Díaz",
+    author_email="<kziete@gmail.com>",
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(),
+    install_requires=["boto3"],
+    keywords=["python", "queue", "sqs"],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+    ],
 )
